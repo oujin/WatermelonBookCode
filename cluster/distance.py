1d99da25aea61e447126db6522c4b0968beec550
oujin <18813102633@163.com>
2019-11-05 18:27:37 +0800

    update

--- cluster/distance.py
+++ cluster/distance.py
@@ -15,7 +15,64 @@ def euclidean_distance(x, y):
         the euclidean distance from each one in x to y,
         e.g. array([d1,...,dn])
     """
-    return np.sqrt(np.sum((x - y) ** 2, -1))
+    return minkowski_distance(x, y)
+
+
+def minkowski_distance(x, y, p=2):
+    """
+    Parameters:
+    ----------
+    x: m-D array-like
+        a cluster points, e.g. array([[x1,y1],...,[xn,yn]])
+    y: 1-D array-like
+        one point, e.g. array([x1,y1]).
+    p: int or np.inf
+        subscrpit of the L-p norm, p >= 1.
+    Returns:
+    ----------
+    dist: 1-D array-like
+        the minkowski distance from each one in x to y,
+        e.g. array([d1,...,dn])
+    """
+    if p < 1:
+        raise("p is too less than 1!")
+    if p < np.inf:
+        return (np.sum((x - y) ** p, -1)) ** (1/p)
+    return np.max(np.abs((x - y)), -1)
+
+
+def manhattan_distance(x, y):
+    """
+    Parameters:
+    ----------
+    x: m-D array-like
+        a cluster points, e.g. array([[x1,y1],...,[xn,yn]])
+    y: 1-D array-like
+        one point, e.g. array([x1,y1]).
+    Returns:
+    ----------
+    dist: 1-D array-like
+        the manhattan distance from each one in x to y,
+        e.g. array([d1,...,dn])
+    """
+    return minkowski_distance(x, y, p=1)
+
+
+def chebyshev_distance(x, y):
+    """
+    Parameters:
+    ----------
+    x: m-D array-like
+        a cluster points, e.g. array([[x1,y1],...,[xn,yn]])
+    y: 1-D array-like
+        one point, e.g. array([x1,y1]).
+    Returns:
+    ----------
+    dist: 1-D array-like
+        the chebyshev distance from each one in x to y,
+        e.g. array([d1,...,dn])
+    """
+    return minkowski_distance(x, y, p=np.inf)
 
 
 if __name__ == "__main__":
@@ -26,3 +83,8 @@ if __name__ == "__main__":
     ])
     y = np.array([1, 1])
     print(euclidean_distance(x, y))
+    print(minkowski_distance(x, y, 2))
+    print(manhattan_distance(x, y))
+    print(minkowski_distance(x, y, 1))
+    print(chebyshev_distance(x, y))
+    print(minkowski_distance(x, y, np.inf))
